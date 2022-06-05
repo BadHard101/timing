5288b335c0451aa9c39e6c4a8984da48c70651f7
Андрей Кузнецов <and.kyz.mir@gmail.com>
2022-06-06 01:28:24 +0300

    Добавьте файлы проекта.

---
+++ timing/timing.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+void func(){}
+
+int main()
+{
+	unsigned int start_time;
+	unsigned int end_time;
+
+	start_time = clock();
+	func();
+	end_time = clock();
+
+	cout << "time: "
+		<< (end_time - start_time) / 1000.0
+		<< " sec\n\n";
+}
